d80c0452fd3106060d1e4299c961760ac66686e5
Banitongwa David <davidbanitongwa@gmail.com>
2022-12-28 21:08:18 +0100

    search fix bug

--- src/main/java/com/example/openocr/HelloServlet.java
+++ src/main/java/com/example/openocr/HelloServlet.java
@@ -2,25 +2,29 @@ package com.example.openocr;
 
 import java.io.*;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+//@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet()
 public class HelloServlet extends HttpServlet {
-    private String message;
+   /* private String message;
 
     public void init() {
         message = "Hello World!";
-    }
+    }*/
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        //response.setContentType("text/html");
 
         // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+//        PrintWriter out = response.getWriter();
+//        out.println("<html><body>");
+//        out.println("<h1>" + message + "</h1>");
+//        out.println("</body></html>");
+
+        request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 
     public void destroy() {
