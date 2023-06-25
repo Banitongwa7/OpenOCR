2b207247549e9e1900737a41469e49a5aba57ce4
Banitongwa David <davidbanitongwa@gmail.com>
2023-06-25 21:53:06 +0100

    update structure

---
+++ next_app/context/DataContext.jsx
@@ -0,0 +1,3 @@
+import React, { createContext } from 'react'
+
+const DataContext = createContext()
--- next_app/tailwind.config.js
+++ next_app/tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
