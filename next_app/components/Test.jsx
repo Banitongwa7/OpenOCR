d8d3f4a04372b83d82e7667e60cf6e66947fcba9
Banitongwa David <davidbanitongwa@gmail.com>
2023-07-01 22:40:04 +0100

    test className

---
+++ next_app/components/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+export default function Test() {
+  return (
+    <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+        <div className='container text-center text-2xl shadow-2xl rounded-lg h-[500px] w-screen'>Page Test</div>
+    </div>
+  )
+}
--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -1,25 +1,12 @@
 "use client";
-import FormInput from "@/components/FormInput"
 import { useState, useEffect } from "react";
-import LoaderStart from "@/components/LoaderStart";
+import Test from "@/components/Test";
 
 export default function Home() {
-  const [load,setLoad] = useState(false)
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setLoad(true);
-    }, 5000);
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, []);
 
   return (
    <>
-   {
-    load ? (<FormInput/>) : (<LoaderStart/>)
-   }
+   <Test />
    </>
   )
 }
